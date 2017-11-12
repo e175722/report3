d321aa811dc925ba8d4dc6387d46cdf8d2789103
Keisuke Tokuhira <e175722@ie.u-ryukyu.ac.jp>
2017-11-12 19:10:37 +0900

    step2 commit

---
+++ src/main/jp/ac/uryukyu/ie/e175722/LivingThing.java
@@ -0,0 +1,45 @@
+package jp.ac.uryukyu.ie.e175722;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+
+    public LivingThing(String name, int hitpoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitpoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitpoint, attack);
+    }
+
+
+    public boolean isDead() {
+        return dead;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+
+    public void attack(LivingThing opponent){
+        if(!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint <= 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
